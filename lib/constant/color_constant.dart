7ff0643abb149058c118334684e65423608da3fe
Charley747474 <Charley747474@outlook.com>
2023-02-11 23:46:17 +0900

    init app

---
+++ lib/constant/color_constant.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class ColorConstant {
+  static Color ogreOdor = Color.fromARGB(255, 230, 218, 216);
+  static Color white = Color(0xFFFFFFFF);
+  static Color black = Color(0xFF00000);
+  static Color riolesOrange = Color(0xFFFA4A0C);
+  static Color tennesseeOrange = Color(0xFFF47B0A);
+}
